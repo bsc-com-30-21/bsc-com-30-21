8dc75658a7ae299a0f8b8e3b54c60a68960bffd1
gondwe3021 <bsc-com-30-21@unima.ac.mw>
2024-04-20 13:21:09 +0200

    second iteration

--- Exercise_1.cpp
+++ Exercise_1.cpp
@@ -2,21 +2,39 @@
 #include <random>
 
 using namespace std;
-int main() {
+
+int generateRandomNumber() {
     // Seed the random number generator
     random_device rd;
     mt19937 gen(rd());
-    
+
     // Define the distribution for random numbers between 0 and 11
     uniform_int_distribution<> dis(0, 11);
-    
-    // Generate and print 10 random numbers
-    cout << "Random numbers between 0 and 11:" << endl;
-    for (int i = 0; i < 10; ++i) {
-        int random_number = dis(gen);
-        cout << random_number << " ";
+
+
+    // Generate random numbers between 0 and 11
+    int daysUntilExpiration = dis(gen);
+    return daysUntilExpiration;
+}
+
+void outputMessage() {
+    int randomDays = generateRandomNumber();
+
+    if (randomDays <= 10 && randomDays > 5) {
+        cout << "Your subscription will expire soon. Renew now!" << endl;
+    } else if (randomDays <= 5 && randomDays > 1) {
+        cout << "Your subscription will expire in " << randomDays << " days. Renew now and save 10%!" << endl;
+    } else if (randomDays == 1) {
+        cout << "Your subscription expires within a day! Renew now and save 20%!" << endl;
+    } else if (randomDays == 0) {
+        cout << "Your subscription has expired" << endl;
+    } else if (randomDays > 10) {
+        cout << "You have an active subscription" << endl;
     }
-    cout << endl;
-    
+}
+
+
+int main() {
+    outputMessage();
     return 0;
 }
