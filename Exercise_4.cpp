5043c1ba929079167c0c429a6e806306f03b1f70
gondwe3021 <bsc-com-30-21@gmail.com>
2024-04-25 01:33:10 +0200

    ok

--- Exercise_4.cpp
+++ Exercise_4.cpp
@@ -1,20 +1,36 @@
-#include <cctype>
 #include <iostream>
+#include <string>
+#include <sstream>
 
 using namespace std;
+
+
+// checks if given string represents an interger
+bool isInteger(const std::string& s) {
+    if (s.empty() || ((!isdigit(s[0])) && (s[0] != '-') && (s[0] != '+'))) return false;
+
+    std::istringstream iss(s);
+    int num;
+    iss >> num;
+
+    return !iss.fail() && iss.eof();
+}
+
 
 int main() {
     int limit = 0;
     while(limit < 5){
-        int userValue;
-        cout << "Enter an integer value between 5 and 10" << endl;
-        cin >> userValue;
+        string userValue;
 
-        if (userValue < 5 && userValue > 10) {
+        cout << "Enter an integer value between 5 and 10" << endl;
+        // read input
+        getline(cin, userValue);
+
+        if (stoi(userValue) < 5 && stoi(userValue) > 10) {
             cout << "You entered " << userValue << " Please enter a value between 5 and 19" << endl;
-        }else if (!isdigit(static_cast<unsigned char>(userValue))) {
+        }else if (!isInteger(userValue)) {
             cout << "Sorry, you entered an invalid number, Please try again" << endl;
-        }else if (userValue >= 5 && userValue <= 10) {
+        }else if (stoi(userValue) >= 5 && stoi(userValue) <= 10) {
             cout << "Your input value " << userValue << " has been accepted" << endl;
             break;
         }
