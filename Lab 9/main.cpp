ffda41c51e9d31b5df090ac61a7776ffaa678adf
benjamin0021 <gondwebenjamin4@gmail.com>
2024-06-07 10:31:13 +0200

    new

--- Lab 9/main.cpp
+++ Lab 9/main.cpp
@@ -4,19 +4,14 @@
 using namespace std;
 
 
-int main()
-{
+int main() {
 Rectangle rect;
 Triangle trgl;
-Polygon poly;
-Polygon* pPoly1 = &rect;
-Polygon* pPoly2 = &trgl;
-Polygon* pPoly3 = &poly;
+Polygon * pPoly1 = &rect;
+Polygon * pPoly2 = &trgl;
 pPoly1->SetValues(4,5);
 pPoly2->SetValues(4,5);
-pPoly3->SetValues(4,5);
-cout << pPoly1->Area() << '\n';
-cout << pPoly2->Area() << '\n';
-cout << pPoly3->Area() << '\n';
+pPoly1->PrintArea();
+pPoly2->PrintArea();
 return 0;
 }
