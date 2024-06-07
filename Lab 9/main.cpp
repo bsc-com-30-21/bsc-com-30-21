dd514296b31362438a8a7c7e23a4488fb51a46ec
benjamin0021 <gondwebenjamin4@gmail.com>
2024-06-07 10:26:38 +0200

    polymorphism

--- Lab 9/main.cpp
+++ Lab 9/main.cpp
@@ -1,19 +1,22 @@
 #include <iostream>
 #include "Rectangle.h"
 #include "Triangle.h"
-
 using namespace std;
 
 
-int main() {
-    Rectangle rect;
-    Triangle trgl;
-
-    rect.SetValues(4,5);
-    trgl.SetValues (4,5);
-
-    cout << rect.Area() << '\n';
-    cout << trgl.Area() << '\n';
-    
-    return 0;
+int main()
+{
+Rectangle rect;
+Triangle trgl;
+Polygon poly;
+Polygon* pPoly1 = &rect;
+Polygon* pPoly2 = &trgl;
+Polygon* pPoly3 = &poly;
+pPoly1->SetValues(4,5);
+pPoly2->SetValues(4,5);
+pPoly3->SetValues(4,5);
+cout << pPoly1->Area() << '\n';
+cout << pPoly2->Area() << '\n';
+cout << pPoly3->Area() << '\n';
+return 0;
 }
